73a5666fb6896c53f384b26cc818e6d5d9477a39
philipiaeveline <philipiaeveline@gmail.com>
2020-11-04 22:05:15 +0300

    add

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,11 @@
 export class Quote{
     public showDescription: boolean;
-    constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+    downVote:number;
+    upVote:number;
+    constructor(public id: number,public name: string,public description: string, public author:string, public completeDate: Date){
       this.showDescription=false;
+      this.downVote=0;
+      this.upVote=0;
     }
   }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,11 +10,11 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
 
 
-    new Quote(2, 'Benard Akaka', 'In three words I can sum up everything  I ve learned about life: it goes on', new Date(2019, 6, 9)),
-    new Quote(3, 'Kerry Kogei', 'It is better to be hated for what  you are than to be loved for what you are not.', new Date(2019, 1, 12)),
-    new Quote(4, 'Philipia', 'It does not do to dwell on dreams and forget to live.', new Date(2019, 11, 18)),
-    new Quote(5, 'Slyvia Anne', 'There are only two ways to live your life. One is as though nothing is a miracle.The other is as though everything is a miracle.', new Date(2020, 11, 3)),
-    new Quote(6, 'Henry Ali', 'Good friends, good books, and a sleepy conscience: this is the ideal life.', new Date(2020+ 3, 14)),
+    new Quote(1, 'Benard Akaka', 'In three words I can sum up everything  I ve learned about life: it goes on', 'henry Kanyoro', new Date(2019, 6, 9)),
+    new Quote(2, 'Kerry Kogei', 'It is better to be hated for what  you are than to be loved for what you are not.', 'Mariga Mwita',new Date(2019, 1, 12)),
+    new Quote(3, 'Philipia', 'It does not do to dwell on dreams and forget to live.', 'Stellah Anne', new Date(2019, 11, 18)),
+    new Quote(4, 'Slyvia Anne', 'There are only two ways to live your life. One is as though nothing is a miracle.The other is as though everything is a miracle.', 'Ken Swalla', new Date(2020, 11, 3)),
+    new Quote(5, 'Henr y Ali', 'Good friends, good books, and a sleepy conscience: this is the ideal life.', 'Dceddy Otieno', new Date(2020+ 3, 14))
   ];
 
   // get sortQuotes() {
@@ -23,14 +23,20 @@ export class QuoteComponent implements OnInit {
   //     return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
   //   });
   // }  
+  upVote(index){
+    this.quotes[index].upVote++;
+  }
+  downVote(index){
+    this.quotes[index].downVote--;
+  }
   addedQuote(quote) {
     let arraysize = this.quotes.length;
     quote.id = arraysize + 1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
   }
-  quoteDelete(isRead, index) {
-    if (isRead) {
+  quoteDelete(isComplete, index) {
+    if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete this Quote?`)
       if (toDelete) {
         this.quotes.splice(index, 1);
@@ -39,8 +45,8 @@ export class QuoteComponent implements OnInit {
     }
   }
 
-  // displayInfo(index){
-  //   this.quotes[index].showInfo = !this.quotes[index].showInfo;
+  //   //  displayInfo(index){
+  //   // this.quotes[index].showInfo = !this.quotes[index].showInfo;
   // }
   constructor() { }
   ngOnInit() {
