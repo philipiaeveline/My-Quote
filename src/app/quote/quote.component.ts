e73d5b4883e2305665d8f32f84b0a3d85c2f7a1e
philipiaeveline <philipiaeveline@gmail.com>
2020-11-04 15:31:22 +0300

    add

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  title = 'welcome to tech quotes';
+  quotes: Quote[] = [
+
+
+    new Quote(2, 'Benard Akaka', 'In three words I can sum up everything  I ve learned about life: it goes on', new Date(2019, 6, 9)),
+    new Quote(3, 'Kerry Kogei', 'It is better to be hated for what  you are than to be loved for what you are not.', new Date(2019, 1, 12)),
+    new Quote(4, 'Philipia', 'It does not do to dwell on dreams and forget to live.', new Date(2019, 11, 18)),
+    new Quote(5, 'Slyvia Anne', 'There are only two ways to live your life. One is as though nothing is a miracle.The other is as though everything is a miracle.', new Date(2020, 11, 3)),
+    new Quote(6, 'Henry Ali', 'Good friends, good books, and a sleepy conscience: this is the ideal life.', new Date(2020+ 3, 14)),
+  ];
+
+  // get sortQuotes() {
+  //   return this.quotes.sort((a, b) => {
+  //     return <any>new Date(b.likes) - <any>new Date(a.likes);
+  //     return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
+  //   });
+  // }  
+  addedQuote(quote) {
+    let arraysize = this.quotes.length;
+    quote.id = arraysize + 1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  quoteDelete(isRead, index) {
+    if (isRead) {
+      let toDelete = confirm(`Are you sure you want to delete this Quote?`)
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+
+    }
+  }
+
+  // displayInfo(index){
+  //   this.quotes[index].showInfo = !this.quotes[index].showInfo;
+  // }
+  constructor() { }
+  ngOnInit() {
+  }
+}
